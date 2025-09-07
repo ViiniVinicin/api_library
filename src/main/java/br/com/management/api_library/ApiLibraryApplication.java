24bf2256fcd083bb9b9b4b618d4bf8c18c6d5588
Erick Vinícius de Oliveira Nascimento <erickvini96@gmail.com>
2025-09-06 23:05:21 -0300

    Initial Commit

---
+++ src/main/java/br/com/management/api_library/ApiLibraryApplication.java
@@ -0,0 +1,13 @@
+package br.com.management.api_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLibraryApplication.class, args);
+	}
+
+}
