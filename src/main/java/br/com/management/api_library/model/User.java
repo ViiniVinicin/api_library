a9eddc5f19113d502a51e219b509756481042ede
Erick Vinícius de Oliveira Nascimento <erickvini96@gmail.com>
2025-09-08 21:20:46 -0300

    Inserindo @Id e @GeneratedValue à variável id

--- src/main/java/br/com/management/api_library/model/User.java
+++ src/main/java/br/com/management/api_library/model/User.java
@@ -1,6 +1,9 @@
 package br.com.management.api_library.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +14,8 @@ import lombok.NoArgsConstructor;
 @Entity
 public class User {
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
