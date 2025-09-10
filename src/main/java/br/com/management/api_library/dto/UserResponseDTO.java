b977de24e9421f15138b77494490dffb620002c1
Erick Vinícius de Oliveira Nascimento <erickvini96@gmail.com>
2025-09-10 05:13:17 -0300

    Refatoração da DTO de Resposta do Usuário de classe para um record

--- src/main/java/br/com/management/api_library/dto/UserResponseDTO.java
+++ src/main/java/br/com/management/api_library/dto/UserResponseDTO.java
@@ -1,15 +1,8 @@
 package br.com.management.api_library.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class UserResponseDTO {
-
-    private Long id;
-    private String username;
-    private String email;
-    private String fullName;
-
-}
+public record UserResponseDTO (
+        Long id,
+        String username,
+        String email,
+        String fullName
+){}
