b6e157766fca20e559eef10bc883a3f7ea6fdd5f
Erick Vinícius <erickvini96@gmail.com>
2025-11-21 13:06:56 -0300

    Configuração de Autenticação por JWT no Swagger

--- src/main/java/br/com/management/api_library/config/OpenApiConfig.java
+++ src/main/java/br/com/management/api_library/config/OpenApiConfig.java
@@ -1,25 +1,36 @@
 package br.com.management.api_library.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        name = "bearerAuth", // Nome do esquema de segurança
+        type = SecuritySchemeType.HTTP, // Tipo HTTP
+        bearerFormat = "JWT", // Formato do token
+        scheme = "bearer" // O prefixo (Bearer)
+)
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API Library - Gerenciamento de Livros")
-                        .description("API para gestão de biblioteca pessoal, com integração ao Google Books.")
+                        .title("API Library")
+                        .description("API de Livraria Pessoal com Spring Security e JWT")
                         .version("1.0")
                         .contact(new Contact()
                                 .name("Seu Nome")
                                 .email("seu.email@exemplo.com")
                         )
-                );
+                )
+                // Adiciona a exigência de segurança globalmente
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
