4a9302c1568eda60bb8db9bb1f46506a00ce42d8
Erick Vinícius <erickvini96@gmail.com>
2026-01-28 06:53:29 -0300

    Modificação de arquivo de código para anotação

--- src/main/java/br/com/management/api_library/config/OpenApiConfig.java
+++ src/main/java/br/com/management/api_library/config/OpenApiConfig.java
@@ -1,39 +1,43 @@
 package br.com.management.api_library.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-// Definição visual do cadeado do JWT
+// 1. Configura o "Cadeado" (Definição Técnica)
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
         scheme = "bearer"
 )
-public class OpenApiConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                // 1. Informações de Capa da Documentação
-                .info(new Info()
-                        .title("Library Management API")
-                        .description("API REST para gerenciamento de biblioteca pessoal. \n" +
-                                "Integração com Google Books, controle de leitura e segurança JWT.")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Vinicius")
-                                .email("vinicius@exemplo.com")
-                        )
+// 2. Configurações Gerais do Documento
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Library Management API",
+                version = "1.0",
+                description = "API REST para gerenciamento de biblioteca pessoal. Integração com Google Books, controle de leitura e segurança JWT.",
+                contact = @Contact(
+                        name = "Erick Vinícius de Oliveira Nascimento",
+                        email = "erickvini96@gmail.com"
                 )
-                // 2. Aplica o cadeado de segurança em todos os endpoints por padrão
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
-    }
+        ),
+        // 3. Aplica o cadeado em TODAS as rotas globalmente
+        security = @SecurityRequirement(name = "bearerAuth"),
+
+        // 4. Definição de Ordem das Pastas (Tags)
+        tags = {
+                @Tag(name = "Autenticação", description = "Login e Tokens"),
+                @Tag(name = "Minha Estante", description = "Gerenciamento da leitura pessoal"),
+                @Tag(name = "Usuários", description = "Gestão de pessoas"),
+                @Tag(name = "Catálogo de Livros", description = "Gestão do acervo global")
+        }
+)
+public class OpenApiConfig {
 }
